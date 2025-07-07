ebf5253211fc8120a0a3feb278663a6d1ad735ec
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-07 17:18:33 +0200

    Simulated new Employee and new subclass of System Developer.

--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -3,5 +3,16 @@ package io.github.bokalebsson;
 public class Main {
     public static void main(String[] args) {
 
+        // Simulating setting up a new Employee:
+        Employee Magnus = new Employee("Magnus Persson",
+                2025, 1, 12);
+        System.out.println(Magnus.getSummary());
+
+        // Simulating setting up a new System Developer:
+        SystemDeveloper Greger = new SystemDeveloper("Greger Pettersson",
+                2025, 1,1,
+                new String[]{"Java", "Python"}, new String[]{"Java", "Python", "C++"});
+        System.out.println(Greger.getSummary());
+
     }
 }
