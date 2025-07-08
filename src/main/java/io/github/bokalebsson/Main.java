d9afdc4958017799778a5fa12e49e2ca7358fe79
BoKalebsson <bo_kalebsson@hotmail.com>
2025-07-08 16:14:00 +0200

    Implemented new getSummary() and toString() for RegularEmployee, and simulated in Main.

--- src/main/java/io/github/bokalebsson/Main.java
+++ src/main/java/io/github/bokalebsson/Main.java
@@ -3,6 +3,9 @@ package io.github.bokalebsson;
 public class Main {
     public static void main(String[] args) {
 
+        // Simulating setting up a new Regular Employee:
+        RegularEmployee Magnus = new RegularEmployee("Magnus Persson", 2025, 1, 13);
+        System.out.println(Magnus.getSummary());
 
         // Simulating setting up a new System Developer:
         SystemDeveloper Greger = new SystemDeveloper("Greger Pettersson",
@@ -15,6 +18,9 @@ public class Main {
                 2025, 3, 24,
                 new String[]{"Jens", "Kalle", "Martin"}, 15);
         System.out.println(Katja.getSummary());
+
+        // Testing toString() as a Regular Employee:
+        System.out.println(Magnus.toString());
 
         // Testing toString() as System Developer:
         System.out.println(Greger.toString());
--- src/main/java/io/github/bokalebsson/RegularEmployee.java
+++ src/main/java/io/github/bokalebsson/RegularEmployee.java
@@ -10,4 +10,25 @@ public class RegularEmployee extends Employee {
     public void calculateSalary() {
         setSalary(25000);
     }
+
+    // Included calculateSalary() here, to always get the correct salary.
+    @Override
+    public String getSummary() {
+        calculateSalary();
+        String baseSummary = super.getSummary();
+        StringBuilder sb = new StringBuilder(baseSummary);
+        sb.append("-- Regular Employee --").append("\n");
+        return sb.toString();
+    }
+
+    // Included calculateSalary() here, to always get the correct salary.
+    @Override
+    public String toString() {
+        calculateSalary();
+        String baseString = super.toString();
+        StringBuilder sb = new StringBuilder(baseString);
+        sb.append("Employee Type: Regular\n");
+        return sb.toString();
+    }
+
 }
